d6866ac160fdbb57671dbb798d0bdde621544723
Darcie <64379406+dspringall@users.noreply.github.com>
2020-12-18 10:50:04 +0100

    Porfolio first deploy

---
+++ home.js
@@ -0,0 +1,24 @@
+// const contactme = document.querySelector('.contact');
+// let elmntToView = document.getElementById("section4");
+
+// // contactme.addEventListener('click', function () {
+// //     alert('clicked')
+// // });
+
+// contactme.onclick.elmntToView.scrollIntoView();
+
+
+// $('img').each(function () {
+
+//     $(this)[0].oncontextmenu = function () {
+
+//         alert('Right-click is not available');
+
+//         return false;
+
+//     };
+
+// });
+
+
+
